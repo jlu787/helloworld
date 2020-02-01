eee36954a45ba93e77fb89709c23fe6a12401cea
jason <jlu787@outlook.com>
2020-02-01 14:18:03 +1300

    Randomised Cubes

---
+++ bombgame/Assets/Scripts/Scr_GenerateObj.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_GenerateObj : MonoBehaviour
+{
+    public enum FaceType
+    {
+        BLANK = 0,
+        BUTTON,       
+    }
+
+    public GameObject FrontFace, BackFace, RightFace, LeftFace, TopFace, BottomFace;
+    public GameObject ButtonPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomiseFaces();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // check current face
+
+        }
+    }
+
+    private void RandomiseFaces()
+    {
+        // decide what goes on each face
+        FaceType front, back, left, right, top, bottom;
+
+        front = (FaceType)Random.Range(0, 2);
+        back = (FaceType)Random.Range(0, 2);
+        left = (FaceType)Random.Range(0, 2);
+        right = (FaceType)Random.Range(0, 2);
+        top = (FaceType)Random.Range(0, 2);
+        bottom = (FaceType)Random.Range(0, 2);
+
+        switch (front)
+        {
+            case FaceType.BLANK:
+                break;
+            case FaceType.BUTTON:
+                Instantiate(ButtonPrefab, FrontFace.transform);
+                break;
+        }
+        switch (back)
+        {
+            case FaceType.BLANK:
+                break;
+            case FaceType.BUTTON:
+                Instantiate(ButtonPrefab, BackFace.transform);
+                break;
+        }
+        switch (left)
+        {
+            case FaceType.BLANK:
+                break;
+            case FaceType.BUTTON:
+                Instantiate(ButtonPrefab, LeftFace.transform);
+                break;
+        }
+        switch (right)
+        {
+            case FaceType.BLANK:
+                break;
+            case FaceType.BUTTON:
+                Instantiate(ButtonPrefab, RightFace.transform);
+                break;
+        }
+        switch (top)
+        {
+            case FaceType.BLANK:
+                break;
+            case FaceType.BUTTON:
+                Instantiate(ButtonPrefab, TopFace.transform);
+                break;
+        }
+        switch (bottom)
+        {
+            case FaceType.BLANK:
+                break;
+            case FaceType.BUTTON:
+                Instantiate(ButtonPrefab, BottomFace.transform);
+                break;
+        }
+    }
+}
